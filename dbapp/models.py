063e62b3f37954cffbb56abee968d2d30decad4e
user <yoonputer@naver.com>
2021-07-22 13:39:45 +0900

    Ad데이터 DB화 완료

--- dbapp/models.py
+++ dbapp/models.py
@@ -9,3 +9,10 @@ class Kakaoshop(models.Model):
 class Navershop(models.Model):
     title = models.CharField(max_length=100)
     create_date = models.CharField(max_length=100)
+
+class Admachine(models.Model):
+    UserID = models.CharField(max_length=100)
+    Gender = models.CharField(max_length=100)
+    Age = models.DecimalField(max_digits=20,decimal_places=5)
+    EstimatedSalary = models.DecimalField(max_digits=20,decimal_places=5)
+    Purchased = models.DecimalField(max_digits=20,decimal_places=5)
