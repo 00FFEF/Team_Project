596d96b25c37f7f3c1522bd52abbac6e34e10d6c
user <yoonputer@naver.com>
2021-07-28 21:42:39 +0900

    머신러닝 테이블 삽입

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from templates.MyAnalysis import MyAnalysis
+import sqlite3
 
 # Create your views here.
 def main(request):
@@ -11,7 +12,22 @@ def scrapping_index(request):
     return render(request,'scrapping_index.html')
 
 def machine_index(request):
-    return render(request,'machine_index.html')
+    result = dict()
+    conn = sqlite3.connect('db.sqlite3')
+    conn.row_factory = sqlite3.Row  # for getting columns
+    curs = conn.cursor()
+    curs.execute('select * from dbapp_admachine da')
+    data = curs.fetchall()
+    for row in data:
+        print(row['Age'])
+        print(row['EstimatedSalary'])
+        print(row['Gender'])
+        print(row['Purchased'])
+        print(row['UserID'])
+        print(row['id'])
+    result['erows'] = data
+
+    return render(request,'machine_index.html',result)
 
 def service_index(request):
     return render(request,'service_index.html')
